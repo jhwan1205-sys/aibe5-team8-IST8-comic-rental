b34a7c4393f7667807460b745d3c45bcc97b72b0
jhwna1205-sys <jhwan1205@gmail.com>
2026-03-15 23:09:10 +0900

    feat/Rq기능구현

--- src/main/java/org/example/Rq.java
+++ src/main/java/org/example/Rq.java
@@ -1,4 +1,29 @@
 package org.example;
 
 public class Rq {
+    private String action;
+    private int[] data;
+    public void Rq(String request) {
+        String[] split = request.split(" ");
+        action = split[0];
+        try {
+            if (split.length == 2) {
+                this.data[0] = Integer.parseInt(split[1]);
+            } else if (split.length == 3) {
+                this.data[0] = Integer.parseInt(split[1]);
+                this.data[1] = Integer.parseInt(split[2]);
+            }
+        } catch (Exception e) {
+            System.out.println("양식에 맞게 명령어를 입력하세요.");
+        }
+
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public int[] getData() {
+        return data;
+    }
 }
