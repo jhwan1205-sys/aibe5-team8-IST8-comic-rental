9355affa8779f6b3a9d91f4d238b930d254d48c2
jhwna1205-sys <jhwan1205@gmail.com>
2026-03-16 17:26:40 +0900

    feat/rq수정

--- src/main/java/org/example/Rq.java
+++ src/main/java/org/example/Rq.java
@@ -3,9 +3,13 @@ package org.example;
 public class Rq {
     private String action;
     private int[] data;
-    public void Rq(String request) {
+    public Rq(String request) {
         String[] split = request.split(" ");
         action = split[0];
+
+        if (split.length > 1) {
+            data = new int[split.length - 1];
+        }
         try {
             if (split.length == 2) {
                 this.data[0] = Integer.parseInt(split[1]);
